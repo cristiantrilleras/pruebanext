9eb4c421d5137aab17ade3930357026cebbc7206
cristian trilleras <cris_01lds@yahoo.es>
2024-02-02 08:33:13 -0500

    Creacion dashboard

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,13 +1,22 @@
-import { fetchCardData, fetchLatestInvoices, fetchRevenue } from "../lib/data"
+
+import { fetchCardData, fetchLatestInvoices } from "../lib/data"
 import LatestInvoices from "../ui/dashboard/latest-invoices";
 import RevenueChart from "../ui/dashboard/revenue-chart";
 import { lusitana } from "../ui/fonts";
+import { Card } from '@/app/ui/dashboard/cards';
+import { Suspense } from 'react';
+import { RevenueChartSkeleton } from '@/app/ui/skeletons';
 
 
 export default async function Page() {
-  const revenue= await  fetchRevenue()
+
   const latestInvoices = await fetchLatestInvoices()
-  fetchCardData
+  const {
+    numberOfInvoices,
+    numberOfCustomers,
+    totalPaidInvoices,
+    totalPendingInvoices,
+  } = await fetchCardData();
   return (
 
     <main>
@@ -15,17 +24,19 @@ export default async function Page() {
         Dashboard
       </h1>
       <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
-        {/* <Card title="Collected" value={totalPaidInvoices} type="collected" /> */}
-        {/* <Card title="Pending" value={totalPendingInvoices} type="pending" /> */}
-        {/* <Card title="Total Invoices" value={numberOfInvoices} type="invoices" /> */}
-        {/* <Card
+        <Card title="Collected" value={totalPaidInvoices} type="collected" />
+        <Card title="Pending" value={totalPendingInvoices} type="pending" />
+        <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
+        <Card
           title="Total Customers"
           value={numberOfCustomers}
           type="customers"
-        /> */}
+        />
       </div>
       <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
-        <RevenueChart revenue={revenue}  />
+      <Suspense fallback={<RevenueChartSkeleton />}>
+          <RevenueChart />
+        </Suspense>
         <LatestInvoices latestInvoices={latestInvoices} />
       </div>
     </main>
